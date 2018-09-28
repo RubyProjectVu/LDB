d873c8fc4fd32903c0203d06e8a544435e5a12a1
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-09-28 21:27:02 +0300

    Update and rename projektas_spec.rb to testai_spec.rb
    Template of this stuff. Nepatikrinta su jokiu toolsu, idetas tik kaip pavyzdys.

---
+++ testai_spec.rb
@@ -0,0 +1,12 @@
+require_relative 'Projektas'
+require 'rspec'
+
+describe Projektas do
+	context "Add stuff to Projektas as needed" do
+		it "Should emit something here" do
+			proj = Projektas.new
+			proj.set_value(3)
+			expect(proj.return_value).to eq 3
+		end
+	end
+end
