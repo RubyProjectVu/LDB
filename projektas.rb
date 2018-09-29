9315bda3bc0354fef7f60aa5794131e8939bb4c9
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-09-29 14:07:23 +0300

    Initial test
    Vienas is keleto testu pasibandymui, kaip veikia. Seems nice.

--- projektas.rb
+++ projektas.rb
@@ -1,10 +1,31 @@
+require 'date'
+
 class Projektas
-	@value = 0
-	def return_value
-		return @value
+	attr_accessor :meta_filename
+	attr_accessor :project_name
+	
+	def initialize(project_name = "Default_project_" + Date.today.to_s, meta_filename = "metadata.txt")
+		@project_name = project_name
+		@meta_filename = meta_filename
+		metafile = File.new(meta_filename, "w")
+		metafile.close
 	end
 	
-	def set_value(number)
-		@value = number
+	def check_metadata
+		outcome = File.file?(@meta_filename)
+		if outcome
+			File.foreach(@meta_filename, "r") {|line| print "Check if #{line} exists"}
+			return true
+		else 
+			return false
+		end
 	end
+	
+	#def save_metadata
+		
+	#end
+	
+	#def tear_down_project
+		
+	#end
 end
--- testai_spec.rb
+++ testai_spec.rb
@@ -2,11 +2,19 @@ require_relative 'projektas'
 require 'rspec'
 
 describe Projektas do
+	
 	context "Add stuff to Projektas as needed" do
 		it "Should emit something here" do
+			#proj = Projektas.new
+			#proj.set_value(3)
+			#expect(proj.return_value).to eq 3
+		end
+	end
+	
+	context "The project opens its metadata file after being created" do
+		it "Should be able to find and open the file" do
 			proj = Projektas.new
-			proj.set_value(3)
-			expect(proj.return_value).to eq 3
+			expect(proj.check_metadata).to be true
 		end
 	end
 end
