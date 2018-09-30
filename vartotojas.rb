0e8424d5b1c2615411a98ca44d3c0ef914e7c900
beijind <beijind@users.noreply.github.com>
2018-09-30 19:06:37 +0300

    Revert "Merge pull request #9 from RubyProjectVu/Aivaras"
    This reverts commit 7eddd99edc535b9735720ce910dc8f41eb1b572e, reversing
    changes made to 662058f90c15d6ab034a67c9c93ec05c4b8a2eea.

--- vartotojas.rb
+++ vartotojas.rb
@@ -1,4 +1,6 @@
 require 'securerandom' #random hash kuriantis metodas yra
+require_relative 'projektas'
+require 'date'
 
 class Vartotojas
 	attr_reader :name
@@ -7,30 +9,26 @@ class Vartotojas
 	attr_reader	:gender
 	attr_reader :user_id
 	attr_reader :phone_number
-	
-	attr_reader :qualification_certificates
-	
+
 	def initialize(name, last_name, email)
 		@name = name
 		@last_name = last_name
 		@email = email
-		@qualification_certificates = Array.new
 	end
-	
+
 	def set_unique_id(id = SecureRandom.hex)
 		@user_id = id
 	end
-	
+
 	def equals(other_user)
 		if (@name == other_user.name and @last_name == other_user.last_name and @email == other_user.email and @user_id == other_user.user_id)
 			return true
 		end
 		return false
 	end
-	
-	def upload_certificate(file)
-		regex = Regexp.new("([a-zA-Z0-9_.\-])+(.doc|.docx|.pdf)$")
-		
-		return regex.match?(file)
+
+	def create_project(project_name, project_file_name)
+		object = Projektas.new(project_name, project_file_name)
+		return object
 	end
 end
