3363d5d0528bb87a3458724883745248cb548899
ErnestasKodis <43343716+ErnestasKodis@users.noreply.github.com>
2018-10-02 02:42:06 +0300

    Update sistema.rb

--- sistema.rb
+++ sistema.rb
@@ -8,15 +8,14 @@ class Sistema
   def initialize
     @logged_in_users = [] # Array.new
   end
-  
-  def user_input_validate(user)
-    #Useris iveda savo Varda pavarde ir el pasta (butina) ir jie turi atitikti reikalavimus
-    validate = true;
-    if !user.name.match(/[A-Z][a-z]+/ )
+
+  def user_input_validation(user)
+    validate = true
+    if !user.name.match(/[a-zA-Z][a-z]+/)
       validate = false
-    elsif !user.last_name.match(/[A-Z][a-z]+/ )
+    elsif !user.last_name.match(/[a-zA-Z][a-z]+/)
       validate = false
-    elsif !user.email.match(/[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z]+/ )
+    elsif !user.email.match(/[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z]+/)
       validate = false
     end
 
@@ -24,46 +23,46 @@ class Sistema
   end
 
   def register(user_to_register)
-    if ( File.file?("users.txt") )
-      File.foreach("users.txt", "r") do |line|
-        users = line.split(";")
-        users.each do |user|
-          user_data = user.split(",")
-          if user_data[2].equal?(user_to_register.email)
-            file.close
-            return false
-          end
-        end
-      end
-    end
-    File.open("users.txt", "a") do |reg|
-      reg.puts "#{user_to_register.name},#{user_to_register.last_name},#{user_to_register.email},#{user_to_register.user_id};"
-      reg.close
-    end
-  end
-
-  def login(user_to_login)
     if File.file?('users.txt')
       File.foreach('users.txt', 'r') do |line|
         users = line.split(';')
         users.each do |user|
           user_data = user.split(',')
-          new_user = Vartotojas.new(name: user_data[0],
-                                    last_name: user_data[1],
-                                    email: user_data[2])
-          new_user.set_unique_id(user_data[3])
-
-          if new_user.equals(user_to_login)
-            unless @logged_in_users.include? user_to_login
-              @logged_in_users.push(user_to_login)
-              return true
-            end
-          end
+          return false if user_data[2].match(user_to_register.email)
         end
       end
-      # false
-    else
-      false
+    end
+    user_to_register.unique_id_setter
+    # File.open("users.txt", "a") do |reg|
+    # reg.puts ";#{user_to_register.name},#{user_to_register.last_name},#{user_to_register.email},#{user_to_register.user_id}"
+    # reg.close
+    # end
+    true
+  end
+
+  def login(user_to_login)
+    if File.file?('users.txt')
+      File.foreach('users.txt', 'r') do |line|
+        return true if construct_user(line.split(';'), user_to_login)
+      end
+    end
+    false
+  end
+
+  def construct_user(line, user_to_login)
+    # users = line.split(';')
+    line.each do |user|
+      user_data = user.split(',')
+      new_user = Vartotojas.new(name: user_data[0],
+                                last_name: user_data[1],
+                                email: user_data[2])
+      new_user.unique_id_setter(user_data[3])
+      if new_user.equals(user_to_login)
+        unless @logged_in_users.include? user_to_login
+          @logged_in_users.push(user_to_login)
+          return true
+        end
+      end
     end
     false
   end
@@ -73,20 +72,18 @@ class Sistema
   end
 
   def log_user_login_logout(name, last_name, logs_in = true)
-    if logs_in
-      File.open('syslog.txt', 'a') do |log|
-        log.puts "User: #{name} #{last_name} logged in at #{Time.now.getutc}."
-      end
-    else
-      File.open('syslog.txt', 'a') do |log|
-        log.puts "User: #{name} #{last_name} logged out at #{Time.now.getutc}."
+    File.open('syslog.txt', 'a') do |log|
+      if logs_in
+        log.puts "User: #{name} #{last_name} logs in at #{Time.now.getutc}."
+      else
+        log.puts "User: #{name} #{last_name} logs out at #{Time.now.getutc}."
       end
     end
   end
 
   def log_project_creation(name, user)
     File.open('syslog.txt', 'a') do |log|
-      v1 = user.get_unique_id
+      v1 = user.unique_id_getter
       v2 = Time.now.getutc
       log.puts "Project: #{name} created by #{v1} at #{v2}."
     end
@@ -94,15 +91,26 @@ class Sistema
 
   def log_password_request(name, last_name, email)
     File.open('syslog.txt', 'a') do |log|
+      sho = [Time.now.getutc, name]
+      # v1 = Time.now.getutc
+      # n = name
+      l = last_name
+      log.puts "Pass req for user: #{sho[1]} #{l} to #{email} at #{sho[0]}."
+    end
+  end
+
+  def log_certificate_upload(name, last_name, file)
+    File.open('syslog.txt', 'a') do |log|
       v1 = Time.now.getutc
       n = name
       l = last_name
-      log.puts "Password request for user: #{n} #{l} to #{email} at #{v1}."
+      f = file
+      log.puts "User: #{n} #{l} uploaded a certification #{f} at #{v1}."
     end
   end
 
   def latest_entry
-    lines = File.readlines('syslog.txt')
-    lines.last
+    File.readlines('syslog.txt').last
+    # lines.last
   end
 end
