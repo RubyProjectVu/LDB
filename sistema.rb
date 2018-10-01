7175dbfa2d7d6d3b66d073217b0e56b1ccaa16e6
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-10-01 20:59:23 +0300

    +fix
    +fix

--- sistema.rb
+++ sistema.rb
@@ -1,6 +1,7 @@
 require_relative 'vartotojas'
 require 'time'
 
+# Documentation
 class Sistema
   attr_reader :logged_in_users
 
@@ -9,30 +10,33 @@ class Sistema
   end
 
   def login(user_to_login)
-    if (File.file?('users.txt'))
+    if File.file?('users.txt')
       File.foreach('users.txt', 'r') do |line|
         users = line.split(';')
         users.each do |user|
           user_data = user.split(',')
-          new_user = Vartotojas.new(name: user_data[0], last_name: user_data[1], email: user_data[2])
+          new_user = Vartotojas.new(name: user_data[0],
+                                    last_name: user_data[1],
+                                    email: user_data[2])
           new_user.set_unique_id(user_data[3])
 
           if new_user.equals(user_to_login)
-            if !@logged_in_users.include? user_to_login
+            unless @logged_in_users.include? user_to_login
               @logged_in_users.push(user_to_login)
               return true
             end
           end
         end
       end
-      return false
+      # false
     else
-      return false
+      false
     end
+    false
   end
 
   def logout(user_to_logout)
-    return @logged_in_users.delete(user_to_logout)
+    @logged_in_users.delete(user_to_logout)
   end
 
   def log_user_login_logout(name, last_name, logs_in = true)
@@ -49,13 +53,18 @@ class Sistema
 
   def log_project_creation(name, user)
     File.open('syslog.txt', 'a') do |log|
-      log.puts "Project: #{name} created by #{user.get_unique_id} at #{Time.now.getutc}."
+      v1 = user.get_unique_id
+      v2 = Time.now.getutc
+      log.puts "Project: #{name} created by #{v1} at #{v2}."
     end
   end
 
   def log_password_request(name, last_name, email)
     File.open('syslog.txt', 'a') do |log|
-      log.puts "Password request for user: #{name} #{last_name} to #{email} at #{Time.now.getutc}."
+      v1 = Time.now.getutc
+      n = name
+      l = last_name
+      log.puts "Password request for user: #{n} #{l} to #{email} at #{v1}."
     end
   end
 
--- testai_spec.rb
+++ testai_spec.rb
@@ -5,28 +5,30 @@ require_relative 'projektas'
 require_relative 'sistema'
 require_relative 'vartotojas'
 require 'rspec'
-require 'securerandom' #random hash kuriantis metodas yra
+require 'securerandom' # random hash kuriantis metodas yra
 require 'etc'
 
 describe Projektas do
-
   context 'A project is validating its metadata, status and owner' do
-    it 'Should be able to find and open the metadata file created after initialising' do
+    it 'Should be able to find and open the metadata file created after init' do
       proj = Projektas.new
       expect(proj.check_metadata).to be true
     end
 
-    it 'Should initially have its owner defined as the user after creation by default' do
+    it 'Should have its owner defined as the user after creation by default' do
       proj = Projektas.new
       expect(proj.parm_manager).to eq Etc.getlogin
       proj.parm_manager('some name')
       expect(proj.parm_manager).to eq 'some name'
     end
 
-    it 'Should check what the status is, whether it is correct, and set/return it' do
+    it 'Should check the status is, whether it is correct, and set/return it' do
       proj = Projektas.new
       expect(proj.parm_project_status).to be nil
-      expect(proj.parm_project_status('ddd')).to eq 'Please set status as one of: ' + ['Proposed', 'Suspended', 'Postponed', 'Cancelled', 'In progress'].join(', ')
+      str1 = 'Please set status as one of: '
+      str2 = ['Proposed', 'Suspended', 'Postponed',
+              'Cancelled', 'In progress'].join(', ')
+      expect(proj.parm_project_status('ddd')).to eq str1 + str2
       proj.parm_project_status('Proposed')
       expect(proj.project_status).to eq 'Proposed'
     end
@@ -65,12 +67,15 @@ describe Projektas do
   context 'A new member is being added to the project' do
     it 'Should return true when a new member is added to the project' do
       proj = Projektas.new
-      expect(proj.add_member(Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com'))).to be true
+      e = 'jhonpeterson@mail.com'
+      usr = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
+      expect(proj.add_member(usr)).to be true
     end
 
-    it 'Should return false when existing member is being added to the project' do
+    it 'Return false when existing member is being added to the project' do
       proj = Projektas.new
-      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com')
+      e = 'jhonpeterson@mail.com'
+      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
       proj.add_member(vart)
       expect(proj.add_member(vart)).to be false
     end
@@ -82,16 +87,18 @@ describe Projektas do
   end
 
   context 'A member is being removed from the project' do
-    it 'Should return true when an existing member gets removed from the project' do
+    it 'True when an existing member gets removed from the project' do
       proj = Projektas.new
-      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com')
+      e = 'jhonpeterson@mail.com'
+      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
       proj.add_member(vart)
       expect(proj.remove_member(vart)).to be true
     end
 
-    it 'Should return false when non-existing member is being removed from the project' do
+    it 'Should return false when nonmember is being removed from the project' do
       proj = Projektas.new
-      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com')
+      e = 'jhonpeterson@mail.com'
+      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
       expect(proj.remove_member(vart)).to be false
     end
 
@@ -103,42 +110,47 @@ describe Projektas do
 end
 
 describe Vartotojas do
-
   context 'User tries to login' do
-
     it 'user1 should be equal to user1' do
-      v1 = Vartotojas.new(name: 'name', last_name: 'lastname', email: 'email@email.com')
+      e = 'email@email.com'
+      v1 = Vartotojas.new(name: 'name', last_name: 'lastname', email: e)
       v1.set_unique_id('123456789')
       expect(v1.equals(v1)).to be true
     end
 
     it 'registered user should be able to login' do
       sys = Sistema.new
-      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')#existing user
+      e = 't@a.com'
+      # existing user
+      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: e)
       v1.set_unique_id('48c7dcc645d82e57f049bd414daa5ae2')
       expect(sys.login(v1)).to be true
     end
 
     it 'unregistered user should not be able to login' do
       sys = Sistema.new
-      v1 = Vartotojas.new(name: 'no', last_name: 'user', email: 't@a.com')#not existing user
+      e = 't@a.com'
+      # not existing user
+      v1 = Vartotojas.new(name: 'no', last_name: 'user', email: e)
       v1.set_unique_id('48c7dcc645d82e57f049bd414daa5ae2')
       expect(sys.login(v1)).to be false
     end
-    
+
     it 'A user should not be able to login if it has  already done that' do
       sys = Sistema.new
-      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')#existing user
+      e = 't@a.com'
+      # existing user
+      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: e)
       v1.set_unique_id('48c7dcc645d82e57f049bd414daa5ae2')
       expect(sys.login(v1)).to be true
       expect(sys.login(v1)).to be false
     end
-    
   end
 
   context 'User tries to logout' do
     it 'should logout if user exists' do
-      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')#existing user
+      # existing user
+      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
       v1.set_unique_id('48c7dcc645d82e57f049bd414daa5ae2')
 
       sys = Sistema.new
@@ -147,81 +159,81 @@ describe Vartotojas do
     end
 
     it 'should return nil on error' do
-      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')#existing user
+      # existing user
+      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
       v1.set_unique_id('48c7dcc645d82e57f049bd414daa5ae2')
       expect(Sistema.new.logout(v1)).to be nil
     end
-    
   end
 
   context 'User uploads qualification certificates' do
     it 'should return true if file is accepted' do
-
-    v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
-    v1.set_unique_id()
-    expect(v1.upload_certificate('file.docx')).to be true
-    expect(v1.upload_certificate('file.doc')).to be true
-    expect(v1.upload_certificate('file.pdf')).to be true
-
+      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
+      v1.set_unique_id
+      expect(v1.upload_certificate('file.docx')).to be true
+      expect(v1.upload_certificate('file.doc')).to be true
+      expect(v1.upload_certificate('file.pdf')).to be true
     end
 
     it 'should return false if file is of wrong format' do
-
-    v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
-    v1.set_unique_id()
-    expect(v1.upload_certificate('file.ff')).to be false
-    expect(v1.upload_certificate('file.exe')).to be false
-    expect(v1.upload_certificate('file.png')).to be false
-    expect(v1.upload_certificate('.gimp')).to be false
-    expect(v1.upload_certificate('.doc')).to be false
-    expect(v1.upload_certificate('.pdf')).to be false
-    expect(v1.upload_certificate('.docx')).to be false
-
+      v1 = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
+      v1.set_unique_id
+      expect(v1.upload_certificate('file.ff')).to be false
+      expect(v1.upload_certificate('file.exe')).to be false
+      expect(v1.upload_certificate('file.png')).to be false
+      expect(v1.upload_certificate('.gimp')).to be false
+      expect(v1.upload_certificate('.doc')).to be false
+      expect(v1.upload_certificate('.pdf')).to be false
+      expect(v1.upload_certificate('.docx')).to be false
     end
   end
 
   context 'Vartotojas creates a new project' do
     it 'Should return true when a new project is created' do
-      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com')
+      e = 'jhonpeterson@mail.com'
+      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
       expect(vart.create_project('Project', 'Project.txt')).to be_truthy
     end
   end
 
   context 'Vartotojas deletes a project' do
     it 'Should return false when nil is being passed to delete_project' do
-      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com')
+      e = 'jhonpeterson@mail.com'
+      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
       expect(vart.delete_project(nil)).to be false
     end
 
     it 'Should return true when project is deleted' do
       proj = Projektas.new
-      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com')
+      e = 'jhonpeterson@mail.com'
+      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
       expect(vart.delete_project(proj)).to be true
     end
 
     it 'Should return false when project is already deleted' do
       proj = Projektas.new
-      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: 'jhonpeterson@mail.com')
+      e = 'jhonpeterson@mail.com'
+      vart = Vartotojas.new(name: 'Jhon', last_name: 'Peterson', email: e)
       vart.delete_project(proj)
       expect(vart.delete_project(proj)).to be false
     end
   end
-  
+
   context 'User asks for a new password' do
-    it 'Should determine whether the email is legit and capable of receiving the link' do
-      sys = Sistema.new
-      usr = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
+    it 'Should determine whether the email is legit' do
+      # sys = Sistema.new
+      e = 't@a.com'
+      usr = Vartotojas.new(name: 'tomas', last_name: 'genut', email: e)
       expect(usr.resend_password_link).to be false
-      usr = Vartotojas.new(name: 'some name', last_name: 'pavardenis', email: 'emailname@gmail.com')
+      e = 'emailname@gmail.com'
+      usr = Vartotojas.new(name: 'some name', last_name: 'pavardenis', email: e)
       expect(usr.resend_password_link).to be true
     end
   end
-
 end
 
 describe Sistema do
-  
-  context 'The system should monitor user loggin in and out activity' do
+  context 'System should monitor user loggin in, out' do
     it 'The system should log a user login or logout' do
       sys = Sistema.new
       usr = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
@@ -234,22 +246,29 @@ describe Sistema do
       expect(sys.get_latest_entry).to start_with 'User: tomas genut '
       expect(sys.get_latest_entry).to include('logged out at')
     end
-    
-    it 'The system should log a password reset link request and see the email and user' do
+
+    it 'The system should log a request and see the email and user' do
       sys = Sistema.new
-      usr = Vartotojas.new(name: 'some name', last_name: 'pavardenis', email: 'emailname@gmail.com')
+      e = 'emailname@gmail.com'
+      usr = Vartotojas.new(name: 'some name', last_name: 'pavardenis', email: e)
       usr.resend_password_link
-      sys.log_password_request('some name', 'pavardenis', 'emailname@gmail.com')
-      expect(sys.get_latest_entry).to start_with 'Password request for user: some name pavardenis to emailname@gmail.com'
+      e = 'emailname@gmail.com'
+      sys.log_password_request('some name', 'pavardenis', e)
+      s1 = 'Password request for user: some name '
+      s2 = 'pavardenis to emailname@gmail.com'
+      expect(sys.get_latest_entry).to start_with s1 + s2
     end
-    
+
     it 'The system should log a project creation' do
       sys = Sistema.new
-      usr = Vartotojas.new(name: 'tomas', last_name: 'genut', email: 't@a.com')
+      e = 't@a.com'
+      usr = Vartotojas.new(name: 'tomas', last_name: 'genut', email: e)
       usr.set_unique_id
       proj = usr.create_project('some name', 'some_meta.txt')
       sys.log_project_creation(proj.parm_project_name, usr)
-      expect(sys.get_latest_entry).to start_with "Project: #{proj.parm_project_name} created by #{usr.get_unique_id} at"
+      s1 = "Project: #{proj.parm_project_name} created "
+      s2 = "by #{usr.get_unique_id} at"
+      expect(sys.get_latest_entry).to start_with s1 + s2
     end
   end
 end
