4591315032b278777eeda93dd587a8858517dbbb
beijind <beijind@users.noreply.github.com>
2018-10-04 13:49:59 +0300

    Remove Debug

--- System.rb
+++ System.rb
@@ -1,11 +1,9 @@
 require_relative 'file_manager/FileManager'
 
 class System
-   attr_reader :test
 
    def initialize
       @FileManager = FileManager.new
-      @test = false
    end
 
 end
--- tests/System_test.rb
+++ tests/System_test.rb
@@ -1,10 +1,6 @@
 require_relative '../System'
 
 describe System do
-   context "Testing" do
-      it "Testing" do
-         s = System.new
-         expect(s.test).to be true
-      end
-   end
+
+
 end
