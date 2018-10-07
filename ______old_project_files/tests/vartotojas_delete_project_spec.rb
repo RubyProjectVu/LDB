9dbc95effcd1d981ed62bdde1e2844ebf9d31112
beijind <beijind@users.noreply.github.com>
2018-10-07 17:15:53 +0300

    remote 1 test

--- ______old_project_files/tests/vartotojas_delete_project_spec.rb
+++ ______old_project_files/tests/vartotojas_delete_project_spec.rb
@@ -1,11 +1,5 @@
 describe Vartotojas do
   context 'when vartotojas deletes a project' do
-    it 'returns false when nil is being passed to delete_project' do
-      e = 'jhonpeterson@mail.com'
-      vart = described_class.new(name: 'Jhon', last_name: 'Peterson', email: e)
-      expect(vart.delete_project(nil)).to be false
-    end
-
     it 'returns true when project is deleted' do
       proj = Projektas.new
       e = 'jhonpeterson@mail.com'
