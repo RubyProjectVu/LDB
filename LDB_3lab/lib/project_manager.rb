5d5be673ba23b78675323af4fe1ce50310f569cd
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-10-27 18:01:44 +0300

    +proj
    +proj

---
+++ LDB_3lab/lib/project_manager.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'date'
+require 'etc'
+require_relative 'project'
+require 'yaml'
+
+# rubocop comment?
+class ProjectManager
+  def initialize
+    @projects = YAML.load_file('projects.yml')
+  end
+
+  def delete_project(id)
+    @projects.delete(id)
+    File.open('projects.yml', 'w') { |fl| fl.write @projects.to_yaml.sub('---', '') }
+  end
+
+  def create_project(project)
+    return false if @projects.key?(project.parm_id)
+
+    hash = project.to_hash
+    File.open('projects.yml', 'a') { |fl| fl.write hash.to_yaml.sub('---', '') }
+  end
+
+  def load_project(id)
+    return false unless @projects.key?(id)
+    proj = @projects.fetch(id)
+    obj = Project.new(project_name: proj.fetch('name'), manager: proj.fetch('manager'), num: id, members: proj.fetch('members'))
+    obj.parm_project_status(proj.fetch('status'))
+    
+    return obj
+  end
+end
