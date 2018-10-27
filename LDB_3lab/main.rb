56655556c9e384acc08a491cca8468bc7707c74b
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-10-27 19:14:23 +0300

    +ui
    +ui

--- LDB_3lab/main.rb
+++ LDB_3lab/main.rb
@@ -4,29 +4,33 @@ require 'tty/prompt'
 require 'time'
 
 #works on LINUX best
+class UI
+  def handle_login
+    cursor = TTY::Cursor
+    prompt = TTY::Prompt.new
+    puts cursor.clear_screen
 
-cursor = TTY::Cursor
-prompt = TTY::Prompt.new
-puts cursor.clear_screen
+    puts cursor.move_to(0,0)
+    puts Rainbow("LDB\t").bright + Rainbow('[' + Date.today.to_s + ']').green
 
-puts cursor.move_to(0,0)
-puts Rainbow("LDB\t").bright + Rainbow('[' + Date.today.to_s + ']').green
+    choice = prompt.select("", %w(Sign\ up Login Exit))
+    puts cursor.clear_lines(2, :up)
 
-choice = prompt.select("", %w(Sign\ up Login Exit))
-puts cursor.clear_lines(2, :up)
-
-case choice
-when 'Sign up'
-  puts cursor.clear_screen
-  prompt.ask('Email:')
-  prompt.mask('Password:')
-  prompt.mask('Repeat password:')
-  puts 'Signing up here'
-when 'Login'
-  puts 'Logging in here'
-else
-  # ...
+    case choice
+    when 'Sign up'
+      puts cursor.clear_screen
+      prompt.ask('Email:')
+      prompt.mask('Password:')
+      prompt.mask('Repeat password:')
+      puts 'Signing up here'
+    when 'Login'
+      puts 'Logging in here'
+    else
+      # ...
+    end
 end
+
+UI.new.handle_login
 
 puts cursor.clear_screen
 #email = gets.chomp
