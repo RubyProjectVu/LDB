f06fd8241313d2c970bab5024c7459bb7b12b326
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-10-28 00:05:29 +0300

    Update main.rb
    looks disgusting

--- main.rb
+++ main.rb
@@ -6,85 +6,65 @@ require_relative 'lib/user'
 require_relative 'lib/user_manager'
 
 #works on LINUX best
-class UI
-  attr_accessor :currentuser
 
-  def mainloop
-    while true do
-      case handle_initial_screen
-      when false
-        return
-      when 'scflogin'
-        handle_user_screen
-        return
-      end
-    end
-  end
+cursor = TTY::Cursor
+prompt = TTY::Prompt.new
+currentuser = nil
+while true do
+	puts cursor.clear_screen
+	puts cursor.move_to(0,0)
+	puts Rainbow("LDB\t").bright + Rainbow('[' + Date.today.to_s + ']').green
 
-  def handle_initial_screen
-    cursor = TTY::Cursor
-    prompt = TTY::Prompt.new
-    puts cursor.clear_screen
+	choice = prompt.select("", %w(Sign\ up Login Exit))
+	puts cursor.clear_lines(2, :up)
+	outcome = nil
 
-    puts cursor.move_to(0,0)
-    puts Rainbow("LDB\t").bright + Rainbow('[' + Date.today.to_s + ']').green
+	case choice
+	when 'Sign up'
+		puts cursor.clear_screen
+      		prompt.ask('Email:')
+      		prompt.mask('Password:')
+      		prompt.mask('Repeat password:')
+      		# handle_sign_up
+    	when 'Login'
+    		puts cursor.clear_screen
+    		usr = User.new(email: currentuser = prompt.ask('Email:'))
+    		usr.password_set(prompt.mask('Password:'))
+		outcome = false
+    		outcome = 'scflogin' if UserManager.new.login(usr.data_getter('email'))
+    		# needs password fetching as well
+   	when 'Exit'
+      		puts cursor.clear_screen
+      		break
+	end
 
-    choice = prompt.select("", %w(Sign\ up Login Exit))
-    puts cursor.clear_lines(2, :up)
+	if outcome.chomp.eql?('scflogin')
+    		while true do
+      			puts cursor.clear_screen
+      			puts cursor.move_to(0,0)
+      			puts Rainbow("LDB ").bright + Rainbow("--[#{@currentuser}]--\t").cyan + Rainbow('[' + Date.today.to_s + ']').green
 
-    case choice
-    when 'Sign up'
-      puts cursor.clear_screen
-      prompt.ask('Email:')
-      prompt.mask('Password:')
-      prompt.mask('Repeat password:')
-      handle_sign_up
-    when 'Login'
-      return handle_login
-    else
-      puts cursor.clear_screen
-      return false
-    end
-    end
+      			#list notes?
+      			#list projects
+      			#list chat?
+      			choice = prompt.select("Modules:", %w(Notes User\ management Project\ management Work\ group\ management Quit))
+      			case choice
+      			when 'Notes'
+        			subchoice = prompt.select("Note actions:", %w(Add\ note Back))
+        			case subchoice
+        			when 'Add note'
+          				puts cursor.clear_lines(2, :up)
+          				prompt.multiline("Edit:")
+          				prompt.yes?('Save this note?')
 
-  def handle_login
-    cursor = TTY::Cursor
-    prompt = TTY::Prompt.new
-    puts cursor.clear_screen
-    email = prompt.ask('Email:')
-    pass = prompt.mask('Password:')
-    usr = User.new(email: email)
-    usr.password_set(pass)
-    @currentuser = email
-    return 'scflogin' if UserManager.new.login(usr.data_getter('email'))
-    # needs password fetching as well
-    false
-  end
+        			when 'Back'
+          				next
+        			end
 
-  def handle_user_screen
-    cursor = TTY::Cursor
-    prompt = TTY::Prompt.new
-    puts cursor.clear_screen
-
-    puts cursor.move_to(0,0)
-    puts Rainbow("LDB ").bright + Rainbow("--[#{@currentuser}]--\t").cyan + Rainbow('[' + Date.today.to_s + ']').green
-
-    #list notes?
-    #list projects
-    #list chat?
-    choice = prompt.select("Modules:", %w(Notes))
-    case choice
-    when 'Notes'
-      subchoice = prompt.select("Note actions:", %w(Add\ note))
-      case subchoice
-      when 'Add note'
-        puts cursor.clear_lines(2, :up)
-        prompt.multiline("Edit:")
-      end
-    end
-    return
-  end
+      			when 'User management'
+      			when 'Quit'
+        			break
+      			end
+    		end
+  	end
 end
-
-ui = UI.new
-ui.mainloop
