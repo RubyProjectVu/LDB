fc54636e5bd9f16e304321053f3e81fcc8a3f75b
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2018-10-27 16:26:32 +0300

    Add files via upload

---
+++ LDB_3lab/lib/user.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'securerandom' # random hash kuriantis metodas yra
+require 'uri'
+
+# Documentation about class User
+class User
+  def initialize(name: '', last_name: '', email: '')
+    @info = { name: name, lname: last_name, email: email,
+              pass: '123', lobject: '' }
+    @projects = {}
+  end
+
+  def data_getter(key)
+    @info.fetch(key.to_sym)
+  end
+  
+  def user_info
+    @info
+  end
+  
+  def password_set(new)
+    # should later (5 laboras) work based on Rails gem 'EmailVeracity'
+    @info[:pass] = new
+  end
+end
---
+++ LDB_3lab/lib/user_manager.rb
@@ -0,0 +1,50 @@
+require_relative 'system'
+require 'yaml'
+
+# class defining user management
+class UserManager
+  def initialize
+    @users = YAML.load_file('users.yml')
+  end
+  
+  def register(user)
+	@current_user = user.user_info
+	
+    mailing = data_getter('email')
+    hash = { mailing => { 'name' => @current_user.fetch('name'.to_sym),
+                          'lname' => @current_user.fetch('lname'.to_sym),
+                          'pwd' => @current_user.fetch('pass'.to_sym) } }
+    return true if users_push(mailing, hash)
+
+    false
+  end
+  
+  def login(user_to_login)
+    return true unless [nil].include?(@users[user_to_login])
+
+    false
+  end
+
+  def delete_user(user)
+    users_pop(user.data_getter('email'))
+  end
+
+  def prepare_deletion
+    return true unless ProjectManager.active_projects_present?
+
+    false
+  end
+  
+  def users_push(email, hash)
+    return false if @users.key?(email)
+
+    File.open('users.yml', 'a') { |fl| fl.write hash.to_yaml.sub('---', '') }
+    true
+  end
+
+  def users_pop(email)
+    @users.delete(email)
+    File.open('users.yml', 'w') { |fl| fl.write @users.to_yaml.sub('---', '') }
+    true
+  end
+end
