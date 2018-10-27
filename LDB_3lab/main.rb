3e943fdfa5b5095a078ed1fd7614a505c9eed9b4
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-10-27 21:11:56 +0300

    +ui
    +ui

--- LDB_3lab/main.rb
+++ LDB_3lab/main.rb
@@ -2,19 +2,24 @@ require 'rainbow'
 require 'tty/cursor'
 require 'tty/prompt'
 require 'time'
+require_relative 'lib/user'
+require_relative 'lib/user_manager'
 
 #works on LINUX best
 class UI
   def mainloop
     while true do
-      case handle_login
+      case handle_initial_screen
       when false
+        return
+      when 'scflogin'
+        # logged in here - take to userpage
         return
       end
     end
   end
 
-  def handle_login
+  def handle_initial_screen
     cursor = TTY::Cursor
     prompt = TTY::Prompt.new
     puts cursor.clear_screen
@@ -31,16 +36,28 @@ class UI
       prompt.ask('Email:')
       prompt.mask('Password:')
       prompt.mask('Repeat password:')
-      #return sing_up
+      handle_sign_up
     when 'Login'
-      #return login
+      return handle_login
     else
       puts cursor.clear_screen
       return false
     end
     end
+
+  def handle_login
+    cursor = TTY::Cursor
+    prompt = TTY::Prompt.new
+    puts cursor.clear_screen
+    email = prompt.ask('Email:')
+    pass = prompt.mask('Password:')
+    usr = User.new(email: email)
+    usr.password_set(pass)
+    return 'scflogin' if UserManager.new.login(usr.data_getter('email'))
+    # needs password fetching as well
+    false
+  end
 end
 
 ui = UI.new
 ui.mainloop
-
