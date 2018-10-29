05d91df7b3717381d2abe6feefd61a8107aac273
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2018-10-29 16:40:15 +0200

    Update user2_spec.rb

--- LDB_3lab/spec/user2_spec.rb
+++ LDB_3lab/spec/user2_spec.rb
@@ -3,38 +3,33 @@
 require 'simplecov'
 SimpleCov.start
 
+require_relative '../lib/user_manager'
 require_relative '../lib/user'
 
-describe User do
+describe UserManager do
   after do
     # Butina - kitaip mutant sumauna users.yml faila ir klasiu kintamuosius.
     hash = { 't@a.com' => { 'name' => 'tomas', 'lname' => 'genut',
-                            'pwd' => '123' } }
+                            'pwd' => '123', 'email' => 't@a.com' } }
     File.open('users.yml', 'w') { |fl| fl.write hash.to_yaml.gsub('---', '') }
   end
 
   it 'user1 should be equal to user1' do
     e = 't@a.com'
-    v1 = described_class.new(email: e)
-    expect(v1.register).to be false
+    v1 = described_class.new
+    expect(v1.register(User.new(email: e))).to be false
   end
 
-  it 'user1 should be 3qual to user1' do
+  it 'user1 should be equal to user1' do
     e = 'ee@a.com'
-    v1 = described_class.new(name: 'tomas', last_name: 'genut', email: e)
-    expect(v1.register).to be true
+	user = User.new(name: 'tomas', last_name: 'genut', email: e)
+    v1 = described_class.new
+    expect(v1.register(user)).to be true
   end
 
   it do
     e = 't@a.com'
-    v1 = described_class.new(email: e)
-    expect(v1.delete_user).to be true
+    v1 = described_class.new
+    expect(v1.delete_user(User.new(email: e))).to be true
   end
-
-  #it 'unregistered user should not be able to login' do
-   # e = 't@t.com'
-    # not existing user
-    #described_class.new(name: 'no', last_name: 'user', email: e)
-    #expect(System.new.login(e)).to be false
-  #end
 end
