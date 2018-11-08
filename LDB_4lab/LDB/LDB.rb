9e63e139d792bc48dc7090305ec30bc5bda10758
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2018-11-08 11:10:16 +0200

    +schema fix
    +schema fix

--- LDB_4lab/LDB/LDB.rb
+++ LDB_4lab/LDB/LDB.rb
@@ -5,14 +5,16 @@ require_relative 'user'
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: ':memory:'
+  database: './dbfile'
 )
 
 ActiveRecord::Schema.define do
-  create_table :projects do |t|
-    t.string :name
-  end
-  create_table :users do |t|
-    t.string :name
+  if !['projects', 'users'].all? { |i| ActiveRecord::Base.connection.tables.include? i }
+    create_table :projects do |t|
+      t.string :name
+    end
+    create_table :users do |t|
+      t.string :name
+    end
   end
 end
