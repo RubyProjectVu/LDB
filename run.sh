805bc9da18f30abfcc15df4bbef3c914475afdab
beijind <beijind@users.noreply.github.com>
2018-11-17 01:06:08 +0200

    Revert "Tests Run FIle and Rubocop YML change"
    This reverts commit 67d7a72c008e1e804026f4b2b668feaf4f297230.

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+bundle install
+
+# Project ProjectManager User UserManager WorkGroup WorkGroupManager BudgetManager NotesManager Search
+bundle exec mutant --include lib --use rspec Project ProjectManager User UserManager WorkGroup WorkGroupManager BudgetManager NotesManager Search -j 1
+
