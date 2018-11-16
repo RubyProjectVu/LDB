165fa30151664150ff46f65b48bd7781b4c1b274
beijind <beijind@users.noreply.github.com>
2018-11-17 01:10:57 +0200

    run tests file

--- LDB_4lab/DEMO/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-bundle install
-
-# Project ProjectManager User UserManager WorkGroup WorkGroupManager BudgetManager NotesManager Search
-bundle exec mutant --include app --use rspec Project ProjectManager User UserManager WorkGroup WorkGroupManager BudgetManager NotesManager Search -j 1
-
